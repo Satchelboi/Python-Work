247d422c772a8d7672e040f3de6753bcb964380d
Satchelboi <msmondazzi@gmail.com>
2018-04-18 00:02:52 -0400

    Continued Work

--- Project 2 Time Sheets.py
+++ Project 2 Time Sheets.py
@@ -32,10 +32,11 @@ def get_times():
     while choice_main == 'y' or choice_main == 'Y':
         choice = input('Please enter the number for your choice:\n 1. List Employees\n 2. Select Employee\n')
         if choice == '1':
-            print(namesheet)
-        if choice == '2':
+            emp_list()
+            print()
+        if choice == '2': # Still pseudocode-ish, re-visit and iron out functionality
             employee = input('Enter employee name: ')
-            for line in namesheet:
+            for line in namesheet: # Statement not currently functional
                 if employee in line:
                     print(line)
         choice_main = input('Do you want to do select anything else? [Y]es or [N]o: ')
@@ -44,9 +45,21 @@ def get_times():
 
 def indv_cost():
     os.system('cls')
+    choice_main = 'y'
+    namesheet = open('pay_rate.txt', 'r') # Juggling too many files, try to condense and sort into fewer
+    while choice_main == 'y' or choice_main == 'Y':
+        menu_choice = input('1. View Employee list.\n 2. Enter employee name.\n')
+        if menu_choice == '1':
+            emp_list()
+            print()
+        if menu_choice == '2':
+            emp_fetch = input('Enter the name of the employee you wish to see the weekly cost of: ')
+#           Find employee name in file and print out name with set pay rate
+#           Pseudocode now, revise once files are condensed and organized
+        choice_main = input('Would you like to perform anything else? [Y]es [N]o: ')
     return False
 
-def week_cost():
+def week_cost(): # Function written in pseudocode right now, fix once files are properly condensed
     os.system('cls')
     return False
 
@@ -57,5 +70,12 @@ def emp_entry():
 def emp_view():
     os.system('cls')
     return False
+
+def emp_list(): # Added to reduce redundant code between other functions
+    os.system('cls')
+    name_list = open('names.txt', 'r')
+    name_read = name_list.read()
+    name_list.close()
+    return name_read
 
 main()
